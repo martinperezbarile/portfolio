22c14f2a40c67d12af15669c22f57e20d5b6da20
Martin Perez Barile <103978967+martinperezbarile@users.noreply.github.com>
2024-11-16 18:04:50 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,78 @@
+function toggleExpand13() {
+    const archivo = document.querySelector('#archivo-13');
+    const etiqueta = document.querySelector('#etiqueta-13');
+    archivo.classList.toggle('expandido');
+    etiqueta.classList.toggle('etiqueta-expandida');
+}
+function toggleExpand12() {
+    const archivo = document.querySelector('#archivo-12');
+    const etiqueta = document.querySelector('#etiqueta-12');
+    archivo.classList.toggle('expandido');
+    etiqueta.classList.toggle('etiqueta-expandida');
+}
+function toggleExpand11() {
+    const archivo = document.querySelector('#archivo-11');
+    const etiqueta = document.querySelector('#etiqueta-11');
+    archivo.classList.toggle('expandido');
+    etiqueta.classList.toggle('etiqueta-expandida');
+}
+function toggleExpand10() {
+    const archivo = document.querySelector('#archivo-10');
+    const etiqueta = document.querySelector('#etiqueta-10');
+    archivo.classList.toggle('expandido');
+    etiqueta.classList.toggle('etiqueta-expandida');
+}
+function toggleExpand9() {
+    const archivo = document.querySelector('#archivo-9');
+    const etiqueta = document.querySelector('#etiqueta-9');
+    archivo.classList.toggle('expandido');
+    etiqueta.classList.toggle('etiqueta-expandida');
+}
+function toggleExpand8() {
+    const archivo = document.querySelector('#archivo-8');
+    const etiqueta = document.querySelector('#etiqueta-8');
+    archivo.classList.toggle('expandido');
+    etiqueta.classList.toggle('etiqueta-expandida');
+}
+function toggleExpand7() {
+    const archivo = document.querySelector('#archivo-7');
+    const etiqueta = document.querySelector('#etiqueta-7');
+    archivo.classList.toggle('expandido');
+    etiqueta.classList.toggle('etiqueta-expandida');
+}
+function toggleExpand6() {
+    const archivo = document.querySelector('#archivo-6');
+    const etiqueta = document.querySelector('#etiqueta-6');
+    archivo.classList.toggle('expandido');
+    etiqueta.classList.toggle('etiqueta-expandida');
+}
+function toggleExpand5() {
+    const archivo = document.querySelector('#archivo-5');
+    const etiqueta = document.querySelector('#etiqueta-5');
+    archivo.classList.toggle('expandido');
+    etiqueta.classList.toggle('etiqueta-expandida');
+}
+function toggleExpand4() {
+    const archivo = document.querySelector('#archivo-4');
+    const etiqueta = document.querySelector('#etiqueta-4');
+    archivo.classList.toggle('expandido');
+    etiqueta.classList.toggle('etiqueta-expandida');
+}
+function toggleExpand3() {
+    const archivo = document.querySelector('#archivo-3');
+    const etiqueta = document.querySelector('#etiqueta-3');
+    archivo.classList.toggle('expandido');
+    etiqueta.classList.toggle('etiqueta-expandida');
+}
+function toggleExpand2() {
+    const archivo = document.querySelector('#archivo-2');
+    const etiqueta = document.querySelector('#etiqueta-2');
+    archivo.classList.toggle('expandido');
+    etiqueta.classList.toggle('etiqueta-expandida');
+}
+function toggleExpand1() {
+    const archivo = document.querySelector('#archivo-1');
+    const etiqueta = document.querySelector('#etiqueta-1');
+    archivo.classList.toggle('expandido');
+    etiqueta.classList.toggle('etiqueta-expandida');
+}
